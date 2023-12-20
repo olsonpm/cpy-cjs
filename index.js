596da44a40b5a7e7485f6d3ac8a6f67aa0c0d9dd
Phil Olson <olsonpm@users.noreply.github.com>
2023-12-20 11:01:59 -0600

    account for default export

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('fix-esm').require('cpy')
+const exportObj = require('fix-esm').require('cpy')
+
+const cpy = exportObj.default
+
+for (const [k, v] of Object.entries(exportObj)) {
+  if (k === 'default') continue
+  cpy[k] = v
+}
+
+module.exports = cpy
